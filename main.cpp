500b639b0470185441bcc43675863228ade7e5c9
MK <qscvdefb@gmail.com>
2024-07-26 13:08:35 +0700

    .

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+void spliptFile() {
+    ifstream inputFile("input.txt");
+    if(!inputFile.is_open())
+        cout << 11;
+}
+
+int main() {
+
+}
