acbf9121d1fa02b48f69137a82c35d559482b6b5
MK <qscvdefb@gmail.com>
2024-07-26 14:25:16 +0700

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ function readFilePromise(filePath, encoding) {
 readFilePromise('input.txt', 'utf8')
     .then(data => {
         let prev = 0
-        const chunkSize = 3000;
         for (let i = 1; i < data.length; i++) {
             if ((i - prev) % 3000 == 0) {
                 while (data[i] != ' ') i--
